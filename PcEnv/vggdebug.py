1e46bc5a6fe0b5f68a8c2b980e5ef17a4dd40638
Chroma7p <nahco3.hcl@gmail.com>
2020-08-17 21:15:02 +0900

    vggishのデバッグ用コードを追加

---
+++ PcEnv/vggdebug.py
@@ -0,0 +1,9 @@
+val=[]
+with open("output")as f:
+    for i in f.readlines():
+        if "value:" in i:
+            val.append(eval(i[17:-1]))
+
+
+data=[np.frombuffer(np.array(i),dtype="int16").tolist()[::2] for i in val]
+print(len(data))
