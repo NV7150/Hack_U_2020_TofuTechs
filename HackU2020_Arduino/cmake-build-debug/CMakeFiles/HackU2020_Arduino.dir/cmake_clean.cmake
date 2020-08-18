078a243b898f0452a42975f3babd2d65e0ac07f4
NV7150 <7150macNV777DAI@gmail.com>
2020-08-18 21:46:41 +0900

    Arduinoのディレクトリを追加

---
+++ HackU2020_Arduino/cmake-build-debug/CMakeFiles/HackU2020_Arduino.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/HackU2020_Arduino.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ HackU2020_Arduino/cmake-build-debug/CMakeFiles/HackU2020_Arduino.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for HackU2020_Arduino.
+# This may be replaced when dependencies are built.
