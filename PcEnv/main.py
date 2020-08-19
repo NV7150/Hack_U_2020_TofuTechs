26239a89e0ca7c00da4f266c4f05305886bba6aa
NV7150 <7150macNV777DAI@gmail.com>
2020-08-19 11:44:22 +0900

    デバイス確認用コードを実装

--- PcEnv/main.py
+++ PcEnv/main.py
@@ -1,9 +1,18 @@
 from PcEnv.RecordProcessor import RecordProcessor
+from PcEnv.AudioRecorder import check_device
 import time
 
-if __name__ == '__main__':
+def process():
     rec_pros = RecordProcessor(10)
     time.sleep(100)
     rec_pros.is_end = True
     rec_pros.record_th.join()
     rec_pros.process_th.join()
+
+def device_check():
+    check_device()
+
+
+if __name__ == '__main__':
+    # process()
+    check_device()
