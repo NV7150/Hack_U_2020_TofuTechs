4e326a3a57b6c2dcb6011eec79184a7a3efc0193
kouichihirachi <kouichi.hirachi@gmail.com>
2020-08-24 17:31:39 +0900

    スマホ連動用プログラム追加

---
+++ PcEnv/RemoteController.py
@@ -0,0 +1,11 @@
+from websocket import create_connection
+import time
+import datetime
+
+# pip install websocket-client をすること
+
+def remoteAction():
+    t_now = datetime.datetime.now().time()
+    ws = create_connection("ws://hacku.australiacentral.cloudapp.azure.com")
+    ws.send('{0}:Action occured!'.format(t_now))
+    ws.close()
