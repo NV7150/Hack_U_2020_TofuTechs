827df34755463711a4398666717ca0c24e0efd48
NV7150 <7150macNV777DAI@gmail.com>
2020-08-26 19:59:05 +0900

    サーキュレータのプログラムを追加

--- HackU2020_Arduino/Circulator.cpp
+++ HackU2020_Arduino/Circulator.cpp
@@ -8,8 +8,15 @@
 #include "Protocols.h"
 
 
+Circulator::Circulator(const Pin &pin): pin(pin.getPinNumber()) {
 
+}
 
 void Circulator::received(byte b) {
-
+    auto com = getProtocolState(PROTOCOL_IMPACT, b);
+    if(com == CORRECT){
+        digitalWrite(pin, HIGH);
+    }else if (com == INCORRECT){
+        digitalWrite(pin, LOW);
+    }
 }
--- HackU2020_Arduino/HackU2020_Arduino.ino
+++ HackU2020_Arduino/HackU2020_Arduino.ino
@@ -5,6 +5,7 @@
 #include "Pins.h"
 #include "Lights.h"
 #include "Punch.h"
+#include "Circulator.h"
 
 using namespace Pins;
 
@@ -25,19 +26,22 @@ Shock *shock;
 Mist *mist;
 Lights* lights;
 Punch* punch;
+Circulator* circulator;
 
 void setup(){
     Serial.begin(115200);
-    receiveMan = new ReceiveManager(4);
+    receiveMan = new ReceiveManager(5);
     shock = new Shock(&MOTOR_PIN, &SOLENOID_PIN);
     mist = new Mist(&FAN_PIN);
     lights = new Lights(LED_RED_PIN, LED_BLUE_PIN, LED_GREEN_PIN);
     punch = new Punch(MOTOR_PUNCH_PIN);
+    circulator = new Circulator(CIRCULATOR_PIN);
 
     receiveMan->registerReceiver(shock);
     receiveMan->registerReceiver(mist);
     receiveMan->registerReceiver(lights);
     receiveMan->registerReceiver(punch);
+    receiveMan->registerReceiver(circulator);
 }
 
 void loop(){
